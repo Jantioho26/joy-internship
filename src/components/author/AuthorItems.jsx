e81e67d17eb450f6995f311c19671e23c9ef5d97
Joy Antioho <j.wildfong@yahoo.com>
2026-09-22 12:16:56 -0400

    fix item detail links across combined app

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -42,7 +42,7 @@ const AuthorItems = ({ items, authorImage }) => {
                     </div>
                   </div>
 
-                  <Link to="/item-details">
+                 <Link to={`/item-details/${item.nftId}`}>
                     <img
                       src={item.nftImage}
                       className="lazy nft__item_preview"
@@ -52,7 +52,7 @@ const AuthorItems = ({ items, authorImage }) => {
                 </div>
 
                 <div className="nft__item_info">
-                  <Link to="/item-details">
+                 <Link to={`/item-details/${item.nftId}`}>
                     <h4>{item.title}</h4>
                   </Link>
 
